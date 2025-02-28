26715142a025660b4a734b75fc5d238cba029803
maxikonnect <tribillio@gmail.com>
2025-02-28 16:35:32 +0000

    footer

--- portfolio_project/index.js
+++ portfolio_project/index.js
@@ -2,3 +2,4 @@ let date = new Date();
 const year = date.getFullYear();
 const footerP = document.querySelector(".footer-nav p");
 footerP.textContent = `@copyright ${year}. All rights reserved`;
+footerP.classList.add("footerPara");
