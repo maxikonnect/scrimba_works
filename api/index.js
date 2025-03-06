3d82d1888176ca9a013d3d569526097f03906eae
Abradu Frimpong Kwame <tribillio@gmail.com>
2025-03-06 15:19:59 -0800

    working on api
    i just learned api from scrimba and i am currently working on it using unsplash

---
+++ api/index.js
@@ -0,0 +1,24 @@
+fetch("https://api.unsplash.com/photos?client_id=YoNKzUFih_Z_mZDLRY7C5ILiJxWaiCB4SiukeBMTh9U")
+    .then(response => response.json())
+    .then(data => {
+        data.map(entry =>{
+            console.log(entry);
+            document.querySelector(".container").innerHTML += `
+                <section class="card">
+                    <div class="card-head">
+                        <img src="${entry.urls.regular}" alt="${entry.alt_description}">
+                    </div>
+                    <div class="card-mid">
+                        <p>Name: ${entry.user.first_name}</p>
+                    </div>
+                    <div class="foot">
+                        <p>Likes: <span>${entry.likes}</span></p>
+                    </div>
+                    <hr />
+                </section>
+            
+            `
+
+
+        }).join("");
+    })
