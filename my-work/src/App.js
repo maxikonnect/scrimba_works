0b6ad00a55ceb8d93a9067f9cc01bf33250c1c85
Abradu Frimpong Kwame <tribillio@gmail.com>
2025-03-09 03:16:18 -0700

    working on more react
    i am working more on react

--- my-work/src/App.js
+++ my-work/src/App.js
@@ -1,14 +1,46 @@
-function MyButton(){
-  return (
-    <button className="btn">Click Me</button>
-  )
+import Teacher from "./public/assets/images/teacher.jpg"
+const name = {
+  user : "Paul Sawer",
+  age: 35,
 }
 
 export default function App(){
   return (
     <>
-      <h1>My App</h1>
-      <MyButton />
+      <img src={Teacher} alt="beautiful teacher teaching" />
+      <h1>{name.user}</h1>
+      <p>{name.age}</p>
     </>
+    
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+/* Test 1 */
+// import "./App.css"
+
+// function MyButton(){
+//   return (
+//     <button className="btn">Click Me</button>
+//   )
+// }
+// const name = 'John'
+// export default function App(){
+//   return (
+//     <div className="container">
+//       <div class="subcontainer">
+//         <h1>{name}</h1>
+//         <MyButton />
+//       </div>
+//     </div>
+//   )
+// }
