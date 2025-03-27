71c7daf5e9439630a3a2d3cf734006a2bb7d5a11
Abradu Frimpong Kwame <tribillio@gmail.com>
2025-03-26 21:46:11 -0700

    passonetoucj

---
+++ passonetouch/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GetDate from './Date';
+
+const Footer = () =>{
+    return (
+        <footer>
+            <h2>Footer</h2>
+            <GetDate />
+        </footer>
+    )
+}
+
+export default Footer;
